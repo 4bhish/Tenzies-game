53766cf41fc8461a80d8363dc6ab9f18bb18a4c6
4bhish <108976136+4bhish@users.noreply.github.com>
2022-11-17 23:55:17 +0530

    Update App component for Tenzies game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
+import Die from './Die';
 import './App.css';
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Confetti from 'react-confetti';
+
 
 function App() {
+  function allNewDice(){
+    const newDice=[]
+
+    for (let i = 0; i < 10; i++) {
+
+      newDice.push  ( generateNewDie())
+
+    } return newDice
+
+  }
+  
+
+function generateNewDie()
+{
+  return{
+    value:Math.ceil(Math.random() * 6),
+    isHeld:false,
+    id:uuidv4()
+  }
+}
+
+function clickNum(id)
+{
+  setDice(prevDice => prevDice.map(die => {
+    return die.id === id ? {...die,isHeld:!die.isHeld} : die
+  }))
+}
+
+
+
+function handleClick()
+  {
+    
+    if(!tenzies)
+    { 
+      setCounter(prevState => prevState + 1)
+
+      setDice(prevState => prevState.map(die => {
+        return die.isHeld ? die :  generateNewDie()
+      }))
+    } else
+    {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  const [dice,setDice] = React.useState(allNewDice())
+  const newDieFace = dice.map(item => <Die handleDie={()=> clickNum(item.id)}  key={item.id} value={item.value} isHeld={item.isHeld}/>)
+
+  const [tenzies,setTenzies] = React.useState(false)
+  const [counter,setCounter] = React.useState(
+    JSON.parse(localStorage.getItem("counter")|| [0])
+  )
+  
+
+  React.useState(()=>{
+    localStorage.getItem("counter",JSON.stringify(counter),[counter])
+  })
+  React.useEffect(()=>{
+    
+    const EachDice=dice.every(dice => dice.isHeld)
+    const diceFace = dice[0].value
+    const allVal = dice.every(dice => diceFace === dice.value )
+
+    if(EachDice && allVal)
+    {
+      setTenzies(true)
+      setCounter(0)
+      
+    }
+  },[dice])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main> 
+         <div className='header'>
+         <h1 className='heading'>Tenzies</h1>
+          <small className='desc'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</small>
+         </div>
+          <div className="dice-container">
+                {newDieFace}
+            </div>
+            <p>You Have rolled dice <span className='counter' >{counter}</span> times</p>
+            <button className='roll' onClick={handleClick}>{tenzies ? "New Game":"Roll"}</button>
+            {
+              tenzies && <Confetti /> 
+            }
+            
+        </main>
   );
 }
 
---
+++ src/Die.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+}
+  return (
+    <div onClick={props.handleDie} style={styles} className="die-face">
+    <h2 className="die-num">{props.value}</h2>
+</div>
+  )
+}
+
+export default Die
